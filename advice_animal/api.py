3b93fd8fac442d2deb80130d9db9c4854ccd3937
Tim Hatch <tim@timhatch.com>
2024-05-18 08:08:57 -0700

    Multi-project support by default.
    A typical single-project repo will be organized like
    
    ```
    <checkout>/.git
    <checkout>/pyproject.toml
    ```
    
    and is found very quickly.
    
    If there is no `pyproject.toml` in the root, it will look into subdirs
    trying to find them in e.g.
    
    ```
    <checkout>/.git
    <checkout>/first-party/project-a/pyproject.toml
    <checkout>/first-party/project-b/pyproject.toml
    <checkout>/third-party/project-c/pyproject.toml
    ```
    
    Co-Authored-By: Amjith Ramanujam <amjith@netflix.com>

--- advice_animal/api.py
+++ advice_animal/api.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 
 import logging
+
+import os
 from enum import IntEnum
 from pathlib import Path
 from typing import Optional
 
+from pathspec import PathSpec
+from pathspec.patterns.gitwildmatch import GitWildMatchPattern
 from vmodule import VLOG_2
 
 LOG = logging.getLogger(__name__)
@@ -27,9 +31,13 @@ class Env:
     def __init__(self, path: Path) -> None:
         # The (read-only) path representing this environment
         self.path = path
+        self.next_steps: list[str] = []
 
         # A relative path (or None) to a top-level python dir
         self.top_level_dir: Optional[Path] = infer_top_level_dir(path)
+
+        self.repo_root = self._find_repo_root()
+        self.py_projects = self.find_py_projects()
 
     def get(self, func, *args, **kwargs):  # type: ignore[no-untyped-def]
         """
@@ -39,12 +47,88 @@ class Env:
         LOG.log(VLOG_2, "get %s", func.__qualname__)
         return func(*args, **kwargs)  # TODO
 
+    def find_py_projects(self) -> list[Path]:
+        """
+        Find every project in a mono-repo (or not).
+        """
+        # A mono-repo may contain multiple projects.
+        # The project root is  a directory containing a setup.py or pyproject.toml.
+        project_indicators = {"setup.py", "pyproject.toml"}
+        projects = []
+        root = self.repo_root
+        ignore = self.gitignore(root) + PathSpec.from_lines(
+            GitWildMatchPattern, ["__pycache__", "*.egg-info", "*.dist-info", ".venv"]
+        )
+
+        for dirpath, dirnames, filenames in os.walk(self.path):
+            # Ignore directories that are ignored by .gitignore.
+            if ignore.match_file(Path(dirpath).resolve().relative_to(root)):
+                # Don't go any deeper into a directory that is ignored.
+                dirnames.clear()
+                continue
+
+            # If we find a project indicator, we've found a project.
+            if project_indicators.intersection(filenames):
+                projects.append(Path(dirpath).resolve().relative_to(root))
+                # Don't go any deeper into this directory.
+                dirnames.clear()
+
+        if not projects:
+            raise Exception("No python projects found in repo")
+        return projects
+
+    def _find_repo_root(self) -> Path:
+        """
+        Find the project root, looking upward from the given path.
+
+        Looks through all parent paths until either the root is reached, or a directory
+        is found that contains any of :attr:`ROOT_MARKERS`.
+        """
+        root_markers: list[Path] = [Path(".git"), Path(".hg")]
+        real_path = self.path.resolve()
+
+        parents = list(real_path.parents)
+        if real_path.is_dir():
+            parents.insert(0, real_path)
+
+        for parent in parents:
+            if any((parent / marker).exists() for marker in root_markers):
+                return parent
+
+        return self.path.resolve()
+
+    @staticmethod
+    def gitignore(path: Path) -> PathSpec:
+        """
+        Generate a `PathSpec` object for a .gitignore file in the given directory.
+
+        If none is found, an empty PathSpec is returned. If the path is not a directory,
+        `ValueError` is raised.
+        """
+        if not path.is_dir():
+            raise ValueError(f"path {path} not a directory")
+
+        gi_path = path / ".gitignore"
+
+        if gi_path.is_file():
+            lines = gi_path.read_text().splitlines()
+        else:
+            lines = []
+
+        return PathSpec.from_lines(GitWildMatchPattern, lines)
+
 
 class FixConfidence(IntEnum):
     UNSET = 0
     RED = 10
     YELLOW = 20
     GREEN = 30
+
+
+class Mode(IntEnum):
+    check = 10
+    diff = 20
+    apply = 30
 
 
 class BaseCheck:
@@ -77,3 +161,17 @@ class BaseCheck:
         included (you should not generally use scm commands here).
         """
         raise NotImplementedError
+
+    def run(self) -> bool:
+        """
+        Intended for sub-classing. Apply this check's fix in `self.env`.
+
+        Return value is whether it made changes.
+
+        Assume that if a commit is being made that any added, modified or deleted files will
+        automatically be included (you should not generally use scm commands here).
+        """
+        if self.check():
+            self.apply(self.env.path)
+            return True
+        return False
--- advice_animal/workflow.py
+++ advice_animal/workflow.py
@@ -1,15 +1,10 @@
 import logging
 import os
-import shutil
 import subprocess
-import tempfile
-from contextlib import contextmanager
 from pathlib import Path
-from typing import Generator, List, Optional, Union
+from typing import Generator, List, Union
 
 import moreorless.click
-
-from .api import Env
 
 LOG = logging.getLogger(__name__)
 
@@ -20,81 +15,6 @@ def run(cmd: List[Union[str, Path]], check: bool = True) -> str:
     LOG.debug("Ran %s -> %s", cmd, proc.returncode)
     LOG.debug("Stdout: %s", proc.stdout)
     return proc.stdout
-
-
-# TODO support parallel workflow, which will work extremely well for git on
-# fixers that we don't know if they generate changes until after they run.
-class BaseWorkflow:
-    current_branch: Optional[str]
-
-    def __init__(self, env: Env) -> None:
-        self.env = env
-        git_head_path = env.path / ".git" / "HEAD"
-        if git_head_path.exists():
-            self.current_branch = git_head_path.read_text().strip().split("/")[-1]
-        else:
-            self.current_branch = None
-        # TODO consider how to handle dirty working copy
-
-    @contextmanager
-    def work_in_branch(
-        self,
-        branch_name: str,
-        commit_message: str,
-        inplace: bool = False,
-        commit: bool = True,
-    ) -> Generator[Path, None, None]:
-        # TODO: A future option to do commit-per-fix in current working copy
-        # might be nice, right now inplace ignores the commit var.
-        if inplace:
-            cur_cwd = os.getcwd()
-            try:
-                os.chdir(self.env.path)
-                yield self.env.path
-            finally:
-                os.chdir(cur_cwd)
-        else:
-            with tempfile.TemporaryDirectory() as d:
-                # TODO consider --single-branch --no-tags -b main, but that doesn't
-                # appear to be able to check out arbitrary revs or origin/main.
-                run(["git", "clone", self.env.path, d])
-                cur_cwd = os.getcwd()
-                try:
-                    os.chdir(d)
-                    run(
-                        [
-                            "git",
-                            "checkout",
-                            "-b",
-                            branch_name,
-                            f"origin/{self.current_branch}",
-                        ]
-                    )
-                    yield Path(d)
-                    run(["git", "add", "-A"])
-                    if commit:
-                        run(["git", "commit", "-m", commit_message])
-                        run(["git", "push", "-f", "origin", branch_name])
-                        print("pushed", branch_name)
-                    else:
-                        run(["git", "status"])
-                        print(run(["git", "diff", "--cached"]))
-                finally:
-                    os.chdir(cur_cwd)
-
-
-class TestWorkflow(BaseWorkflow):
-    @contextmanager
-    def work_in_branch(
-        self,
-        branch_name: str,
-        commit_message: str,
-        inplace: bool = False,
-        commit: bool = True,
-    ) -> Generator[Path, None, None]:
-        with tempfile.TemporaryDirectory() as d:
-            shutil.copytree(self.env.path, Path(d, "work"))
-            yield Path(d, "work")
 
 
 def files(a: Path) -> Generator[Path, None, None]:
