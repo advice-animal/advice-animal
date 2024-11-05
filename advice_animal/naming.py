f7af4e2cfe8180dfa32f4346746feac0b231b9ea
Tim Hatch <tim@timhatch.com>
2024-11-05 10:08:46 -0800

    Remove subcommands, allow "pave" unnecessarily

---
+++ advice_animal/naming.py
@@ -0,0 +1,8 @@
+import re
+
+
+def advice_name_re(prefix: str) -> str:
+    """
+    returns a regular expression string that matches either prefix/ or prefix as the entire string.
+    """
+    return f"^({prefix}$|{prefix}/.*)$"
