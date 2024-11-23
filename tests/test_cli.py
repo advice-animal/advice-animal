504fbe6514fcb166d044b2f48f9619396eaa9eba
Amjith Ramanujam <amjith@netflix.com>
2024-11-22 16:02:07 -0800

    Add a test for the success message.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+from pathlib import Path
 
 from advice_animal.cli import main
 from click.testing import CliRunner
@@ -28,3 +30,20 @@ def test_list():
     result = runner.invoke(main, ["--preview"])
     assert "* shouty\n* pip-tools - (preview)\n" in result.output
     assert result.exit_code == 0
+
+
+def test_apply(tmp_path):
+    (tmp_path / "README.txt").write_text("Hello")
+    (tmp_path / "pyproject.toml").write_text("")
+    subprocess.check_call(
+        ["git", "init", "--initial-branch", "fancy-branch"], cwd=tmp_path
+    )
+    subprocess.check_call(["git", "add", "-A"], cwd=tmp_path)
+    subprocess.check_call(["git", "commit", "-m", "foo"], cwd=tmp_path)
+    os.environ["ADVICE_DIR"] = str(Path(__file__).parent / "advice")
+
+    os.chdir(tmp_path)
+    runner = CliRunner()
+    result = runner.invoke(main, ["-a"])
+    assert result.exit_code == 0
+    assert result.output == "shouty: Changes made\n"
