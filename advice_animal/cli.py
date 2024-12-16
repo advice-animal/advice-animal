f0fd3285abf7f666d5a3b677af5d90f8eb3da574
Jadon Gaertner <jgaertner@netflix.com>
2024-12-16 09:48:04 -0800

    Descriptions from check class docstrings

--- advice_animal/cli.py
+++ advice_animal/cli.py
@@ -269,9 +269,14 @@ def show_list(ctx: click.Context) -> bool:
     for advice_name, check_cls in runner.order_check_classes(filter=everything):
         if check_cls.confidence.name != "UNSET":
             name = click.style(advice_name, fg=check_cls.confidence.name.lower())
+            description = click.style(check_cls.__doc__, fg=check_cls.confidence.name.lower()) if check_cls.__doc__ else None
         else:
             name = click.style(advice_name, fg="green")
+            description = click.style(check_cls.__doc__, fg="green") if check_cls.__doc__ else None
         click.echo(f"* {name}{' - (preview)' if check_cls.preview else ''}")
+        if description:
+            first_line = description.strip().split('\n')[0]  # Needs to be on a separate line to prevent backslash in f-string
+            click.echo(f"    {first_line}")
     return True
 
 
