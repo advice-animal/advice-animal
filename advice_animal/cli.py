fafdb6f0c0a531262d406dbbe8ce968bea93b603
Tim Hatch <tim@timhatch.com>
2025-02-12 10:26:07 -0800

    Ruff formatting

--- advice_animal/cli.py
+++ advice_animal/cli.py
@@ -267,10 +267,16 @@ def show_list(ctx: click.Context) -> bool:
         name_filter=re.compile(".*"),
     )
     for advice_name, check_cls in runner.order_check_classes(filter=everything):
-        description = check_cls.__doc__.strip().split('\n')[0] if check_cls.__doc__ else None
+        description = (
+            check_cls.__doc__.strip().split("\n")[0] if check_cls.__doc__ else None
+        )
         if check_cls.confidence.name != "UNSET":
             name = click.style(advice_name, fg=check_cls.confidence.name.lower())
-            description = click.style(description, fg=check_cls.confidence.name.lower()) if description else None
+            description = (
+                click.style(description, fg=check_cls.confidence.name.lower())
+                if description
+                else None
+            )
         else:
             name = click.style(advice_name, fg="green")
             description = click.style(description, fg="green") if description else None
--- tests/advice/shouty/__init__.py
+++ tests/advice/shouty/__init__.py
@@ -1,12 +1,15 @@
-from advice_animal import BaseCheck, FixConfidence
+from advice_animal import BaseCheck
 
 import central_lib  # ensure that project imports work
+
 assert central_lib
+
 
 class Check(BaseCheck):
     order = 10
+
     def run(self):
-        p = (self.env.path / "README.txt")
+        p = self.env.path / "README.txt"
         cur = p.read_text()
         new = cur.upper()
         p.write_text(new)
